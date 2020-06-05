76b21711f0b655d949b83fca9df60bd08f5e880b
Rachel S. Lee <rslee0025@gmail.com>
2020-06-05 02:06:56 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,14 +1,12 @@
 class Cat 
   attr_accessor :name 
-end   
-
-Maru = Cat.new 
-Maru.name = "Maru"
-
-class Cat 
+  
   def meow
     puts "meow!"
-  end  
+  end 
 end   
+
+Maru = Cat.new       #we create an instance of a cat called Maru
+Maru.name = "Maru"  #our instance we called Maru, knows its name.
 
 Maru.meow
